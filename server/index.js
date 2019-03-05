e33c7ff96be767ad5792935bcddfa2380981293c
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-03-05 14:00:11 -0700

    folders & index.js

---
+++ server/index.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const massive = require('massive');
+
+// CONTROLLERS
+const ac = require('./controllers/auth_controller');
+const tc = require('./controllers/task_controller');
+const gc = require('./controllers/goal_controller');
+const thxc = require('./controllers/thanks_controller');
+const ec = require ('./controllers/excite_controller');
+const stc = require('./controllers/sub_task_controller');
+const sgc = require('./controllers/sub_goal_controller');
+
+// .ENV
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+// MIDDLEWARE
+const app = express();
+
+app.use(express.json());
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 48
+    }
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+    app.listen(SERVER_PORT, () => {
+        console.log(`${SERVER_PORT} birds flying high!`)
+    })
+})
+
+//// AUTH ENDPOINTS ////
+app.get('/api/current', ac.getUser);
+app.post('/auth/register', ac.register);
+app.post('/api/login', ac.login);
+app.post('/api/logout', ac.logout);
