892f2a7f742ef6dd39deb1512d685963baef8760
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-03-08 18:53:59 -0700

    goals displaying

--- src/components/Goal/Goal.js
+++ src/components/Goal/Goal.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
+import { connect } from 'react-redux';
+
 
 class Goal extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            goals: [],
+            subGoals: []
+        }
+    }
+
+
+
     render() {
+        const { goals } = this.props;
+        let goalArr = goals.map((task, i) => {
+            return(
+                <div>
+                    <h3>{goals[i].name}</h3>
+                </div>
+            )
+        })
+
         return (
             <div className="Goal">
                 <Nav />
                 <h1>Goal Component</h1>
+                <h3>
+                    {goalArr}
+                </h3>
+                <h5></h5>
             </div>
         );
     }
 }
 
-export default Goal;
+const mapStateToProps = (reduxState) => {
+    return {
+        goals: reduxState.data.goals,
+        subGoals: reduxState.data.subGoals
+    }    
+}
+
+export default connect(mapStateToProps)(Goal);
