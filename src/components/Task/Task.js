db517a1707b6b07b35ed78de5c5e61e3c0d7eda8
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-03-08 18:41:22 -0700

    task dispalying

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import Nav from '../Nav/Nav';
+import { connect } from 'react-redux';
 
 
 class Task extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
-
+            tasks: [],
+            subTasks: []
         }
     }
 
 
 
     render() {
+        const { tasks } = this.props;
+        let taskArr = tasks.map((task, i) => {
+            return(
+                <div>
+                    <h3>{tasks[i].name}</h3>
+                </div>
+            )
+        })
+
         return (
             <div className="Task">
                 <Nav />
                 <h1>Task Component</h1>
+                <h3>
+                    {taskArr}
+                </h3>
+                <h5></h5>
             </div>
         );
     }
 }
 
-export default Task;
+const mapStateToProps = (reduxState) => {
+    return {
+        tasks: reduxState.data.tasks,
+        subTasks: reduxState.data.subTasks
+    }    
+}
+
+export default connect(mapStateToProps)(Task);
